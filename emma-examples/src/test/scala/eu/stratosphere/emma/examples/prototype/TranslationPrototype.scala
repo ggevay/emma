8d1031fff4841c0eac084156bc40be75feef10b9
Gabor Gevay <ggab90@gmail.com>
2015-10-28 11:32:09 +0100

    strange Kryo exception

--- emma-examples/src/test/scala/eu/stratosphere/emma/examples/prototype/TranslationPrototype.scala
+++ emma-examples/src/test/scala/eu/stratosphere/emma/examples/prototype/TranslationPrototype.scala
@@ -1,12 +1,33 @@
 package eu.stratosphere.emma.examples.prototype
 
-import eu.stratosphere.emma.api.{DataBag, emma}
+import eu.stratosphere.emma.api._
+import eu.stratosphere.emma.codegen.testschema.IMDBEntry
 import eu.stratosphere.emma.examples.Algorithm
 import eu.stratosphere.emma.runtime
+import eu.stratosphere.emma.testutil._
 
 class TranslationPrototype(rt: runtime.Engine) extends Algorithm(rt) {
 
   def run() = {
+    val algo = emma.parallelize
+    {
+      val imdbTop100 = read(materializeResource("/cinema/imdb.csv"), new CSVInputFormat[IMDBEntry])
+
+      val jj = for {
+        imdb <- imdbTop100
+        that <- imdbTop100
+        if {
+          val IMDBEntry(_, rating, _, _, year) = imdb
+          (year, rating)
+        } == (that.year, that.rating)
+      } yield that
+
+      val out = jj.groupBy(x=>x)
+
+      //write("/tmp/xxx", new CSVOutputFormat[IMDBEntry])(out)
+    }
+
+    algo run rt
   }
 }
 
